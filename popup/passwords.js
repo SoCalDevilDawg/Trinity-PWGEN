02a5923e20e972a73420b88a1797e331e8201563
rob <robblue2x@gmail.com>
2017-05-08 20:06:57 +0100

    add npm and eslint

--- popup/passwords.js
+++ popup/passwords.js
@@ -1,49 +1,46 @@
-const app = document.getElementById("app");
+const app = document.getElementById('app');
 
 const pools = {
-  alpha: "abcdefghijklmnopqrstuwxyzABCDEFGHIJKLMNOPQRSTUWXYZ",
-  num: "abcdefghijklmnopqrstuwxyzABCDEFGHIJKLMNOPQRSTUWXYZ0123456789",
-  sym: "abcdefghijklmnopqrstuwxyzABCDEFGHIJKLMNOPQRSTUWXYZ0123456789!£$%^&*()_+-{}:@?[];#,.",
-}
+  alpha: 'abcdefghijklmnopqrstuwxyzABCDEFGHIJKLMNOPQRSTUWXYZ',
+  num: 'abcdefghijklmnopqrstuwxyzABCDEFGHIJKLMNOPQRSTUWXYZ0123456789',
+  sym: 'abcdefghijklmnopqrstuwxyzABCDEFGHIJKLMNOPQRSTUWXYZ0123456789!£$%^&*()_+-{}:@?[];#,.'
+};
 const poolNames = {
-  alpha: "Alpha",
-  num: "Alpha-numeric",
-  sym: "Alpha-numeric + symbols",
-}
-
+  alpha: 'Alpha',
+  num: 'Alpha-numeric',
+  sym: 'Alpha-numeric + symbols'
+};
 
 const genPassword = (pool, len) => {
-  let array = new Uint32Array(len);
+  const array = new Uint32Array(len);
   window.crypto.getRandomValues(array);
-  let password = "";
-  let nums = "";
+  let password = '';
+  let nums = '';
   for (let i = 0; i < array.length; i++) {
     password += pool[array[i] % pool.length];
-    nums += array[i] % pool.length + ",";
+    nums += `${array[i] % pool.length},`;
   }
   return password;
-}
+};
 
-const copyPassword = (pool, len) => {
-  return () => {
-    let a = document.createElement("input");
-    a.value = genPassword(pool, len);
-    app.appendChild(a);
-    a.select();
-    document.execCommand("Copy");
-    app.removeChild(a);
-    window.close();
-  }
-}
+const copyPassword = (pool, len) => () => {
+  const a = document.createElement('input');
+  a.value = genPassword(pool, len);
+  app.appendChild(a);
+  a.select();
+  document.execCommand('Copy');
+  app.removeChild(a);
+  window.close();
+};
 
-for (let c of ["alpha", "num", "sym"]) {
-  let a = document.createElement("div");
+for (const c of ['alpha', 'num', 'sym']) {
+  const a = document.createElement('div');
   a.innerText = poolNames[c];
   app.appendChild(a);
 
-  let count = document.createElement("div");
+  const count = document.createElement('div');
   for (let i = 8; i <= 32; i += 4) {
-    let e = document.createElement("button");
+    const e = document.createElement('button');
     e.innerText = i;
     e.onclick = copyPassword(pools[c], i);
     count.appendChild(e);
