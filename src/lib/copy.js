fef172d5ce2653ae47983703a142adf8e84cfd50
rob <r.armstrong@camlintechnologies.com>
2017-10-04 13:46:26 +0100

    fix: make input not display when copying password

--- src/lib/copy.js
+++ src/lib/copy.js
@@ -1,6 +1,7 @@
 module.exports = text => {
   const a = document.createElement('input');
   a.value = text;
+  a.style = { display: 'none' };
   document.body.appendChild(a);
   a.select();
   document.execCommand('Copy');
